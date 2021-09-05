cbe1681cde99776d6bf31042726ac8f8389f85ac
Daniel <dongyuchiao@gmail.com>
2021-09-05 22:26:39 +0800

    just test

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-footer style="text-align: center">
-    deusyu.xyz 111<span v-show="user.id">，欢迎：{{user.name}}</span>
+    deusyu.xyz 111111<span v-show="user.id">，欢迎：{{user.name}}</span>
   </a-layout-footer>
 </template>
 
