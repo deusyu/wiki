01635d74cd4ef3ca2a4d3693b8124c0165a5fe8a
Daniel <dongyuchiao@gmail.com>
2021-09-05 22:17:34 +0800

    修改阿里云为搬瓦工

--- web/src/views/aliyun.vue
+++ web/src/views/aliyun.vue
@@ -2,19 +2,19 @@
   <a-layout>
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
       <div class="aliyun">
-        <h2>阿里云购买指导优惠</h2>
-        <div><b>如果你还没用过云服务，那就落伍了！</b></div>
+        <h2>搬瓦工购买指导优惠</h2>
+        <div><b>如果你还没用过VPS，那就落伍了！</b></div>
         <div><b>每个程序员都应该有一个自己的线上作品</b></div>
         <div><b>可以是博客、在线笔记，WIKI知识库等，用于技术积累，面试加分</b></div>
         <br>
-        <div style="color: rgb(249, 150, 59)"><b>阿里云选购指导、优惠、代金券，可联系QQ：82144921</b></div>
+        <div style="color: rgb(249, 150, 59)"><b>其中优惠力度最大的搬瓦工优惠码可获 6.58% 的循环折扣，只需要在“Promotional Code”处填入上面的搬瓦工优惠码即可获得折扣</b></div>
         <div style="color: rgb(249, 150, 59)"><b>自助优惠：
-          <a href="https://partner.aliyun.com/shop/1704506477397431" target="_blank">https://partner.aliyun.com/shop/1704506477397431</a></b>
+          <a href="https://bandwagonhost.com/aff.php?aff=34745" target="_blank">https://bandwagonhost.com/aff.php?aff=34745</a></b>
         </div>
         <br>
-        <div style="color: rgb(194, 79, 74)"><b>还没注册过阿里云的可以先QQ联系，灰常便宜！！！</b></div>
-        <div>
-          <img src="http://www.jiawablog.com/image/aliyun1.jpg">
+        <div style="color: rgb(194, 79, 74)"><b>还没购买搬瓦工的可以先telegram联系，非常便宜！！！</b></div>
+        <div >
+          <img style="width: 55vw; min-width: 330px;" src="https://bloggerback-1259552297.cos.ap-chengdu.myqcloud.com/%E7%99%BD%E5%A1%94wiki%E7%9F%A5%E8%AF%86%E5%BA%93/iShot2021-09-05%2022.10.52.png">
         </div>
       </div>
     </a-layout-content>
